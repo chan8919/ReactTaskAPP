23da3ce2ddabc89718470dea80aec4ad45f48a67
Sori <chan8919@naver.com>
2025-06-19 02:52:28 +0900

    firebase를 통한 구글 로그인 기능 추가

--- src/components/BoardList/BoardList.tsx
+++ src/components/BoardList/BoardList.tsx
@@ -1,59 +1,109 @@
-import React, { useState, type FC } from 'react'
-import { useTypedSelector } from '../../hooks/redux';
-import SideForm from './SideForm/SideForm';
-import { FiPlusCircle } from 'react-icons/fi';
-import { addButton, addSection, boardItem, boardItemActive, container, title } from './BoardList.css';
-import clsx from 'clsx';
+import React, { useState, type FC } from "react";
+import { useTypedDispatch, useTypedSelector } from "../../hooks/redux";
+import SideForm from "./SideForm/SideForm";
+import { FiLogIn, FiPlusCircle } from "react-icons/fi";
+import {
+  addButton,
+  addSection,
+  boardItem,
+  boardItemActive,
+  container,
+  title,
+} from "./BoardList.css";
+import clsx from "clsx";
+import { GoSignOut } from "react-icons/go";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { app } from "../../firebase";
+import { removeUserState, setUser } from "../../store/slices/userSlice";
+import { useAuth } from "../../hooks/useAuth";
 
-type TBoardListProps={
+type TBoardListProps = {
   activeBoardId: string;
   setActiveBoardId: React.Dispatch<React.SetStateAction<string>>;
-}
+};
 
-const BoardList:FC<TBoardListProps> = ({activeBoardId,setActiveBoardId}) => {
-
-  const {boardArray} = useTypedSelector(state=>state.boards);
+const BoardList: FC<TBoardListProps> = ({
+  activeBoardId,
+  setActiveBoardId,
+}) => {
+  const { boardArray } = useTypedSelector((state) => state.boards);
   const [isFormOpen, setIsFormOpen] = useState(false);
-  const handleClick=()=>{
+  const dispatch = useTypedDispatch();
+
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const { isAuth } = useAuth();
+
+  const handleClick = () => {
     setIsFormOpen(!isFormOpen);
-  }
+  };
+  const handleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then((userCredential) => {
+        console.log(userCredential);
+        dispatch(
+          setUser({
+            email: userCredential.user.email,
+            id: userCredential.user.uid,
+          })
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleLogOut = () => {
+    signOut(auth)
+      .then(() => {
+        dispatch(removeUserState());
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
     <div className={container}>
-      <div className={title}>
-        게시판:
-      </div>
-      {boardArray.map((board,index)=>(
-        <div key={board.boardId}
-        onClick={()=>setActiveBoardId(boardArray[index].boardId)}
-        className={
-          clsx(
+      <div className={title}>게시판:</div>
+      {boardArray.map((board, index) => (
+        <div
+          key={board.boardId}
+          onClick={() => setActiveBoardId(boardArray[index].boardId)}
+          className={clsx(
             {
               [boardItemActive]:
-              boardArray.findIndex(b=>b.boardId===activeBoardId)===index,
+                boardArray.findIndex((b) => b.boardId === activeBoardId) ===
+                index,
             },
             {
               [boardItem]:
-              boardArray.findIndex(b=>b.boardId===activeBoardId)!==index,
+                boardArray.findIndex((b) => b.boardId === activeBoardId) !==
+                index,
             }
-          )
-        } 
+          )}
         >
-          <div>
-            {board.boardName}
-            </div>
+          <div>{board.boardName}</div>
         </div>
       ))}
       <div className={addSection}>
-        {
-          isFormOpen ?
-          <SideForm setIsFormOpen={setIsFormOpen}/>
-          :
-          <FiPlusCircle className={addButton} onClick={handleClick}/>
-        }
-
+        {isFormOpen ? (
+          <SideForm setIsFormOpen={setIsFormOpen} />
+        ) : (
+          <FiPlusCircle className={addButton} onClick={handleClick} />
+        )}
+        {isAuth ? (
+          <GoSignOut className={addButton} onClick={handleLogOut} />
+        ) : (
+          <FiLogIn className={addButton} onClick={handleLogin} />
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BoardList
+export default BoardList;
---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDcp4h11Uo2UzhwK0xs772hLmD-81lbhxU",
+  authDomain: "todotask-4e370.firebaseapp.com",
+  projectId: "todotask-4e370",
+  storageBucket: "todotask-4e370.firebasestorage.app",
+  messagingSenderId: "394407494739",
+  appId: "1:394407494739:web:0cfc08b944003fa90a32a6",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
